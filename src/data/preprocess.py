4b1dbd1c623b7a00c0829c817e927df1259ad189
Rjangam999 <rishijangam777@gmail.com>
2026-01-01 16:08:52 +0530

    Added text preprocessing module

---
+++ src/data/preprocess.py
@@ -0,0 +1,18 @@
+import re 
+import string 
+
+def clean_text(text: str) -> str: 
+    # lower case 
+    text = text.lower()
+    # URLs removal 
+    text = re.sub(r'http\S+', '', text)
+    # @mentions removal 
+    text = re.sub('@\w+','',text)
+    # hastag removal 
+    text = re.sub(r'#\w+', '', text)    
+    # remove panctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # remove digits 
+    text = re.sub(r'\d+', '' , text)
+    # remove white spaces 
+    return text.strip()
